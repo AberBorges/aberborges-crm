fade95e33e8a96cd211502d6f3b5ce271df79cd6
AberBorges <aberturasborgesapp@gmail.com>
2026-06-01 12:00:06 -0300

    Delete src directory

--- src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://capfyvyskyzgtmsjdzkv.supabase.co'
-const SUPABASE_KEY = 'sb_publishable_om_mrNa9Grd2KM_apBkTOQ_2AsOQN6D'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
