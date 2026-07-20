1ab2495ef41900d2bf6e514cdd4edd16f4f473e4
AberBorges <aberturasborgesapp@gmail.com>
2026-07-19 23:16:24 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// v5 - offline localStorage sync
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    build: {
+          rollupOptions: {
+                  output: {
+                            entryFileNames: `assets/[name]-[hash].js`,
+                            chunkFileNames: `assets/[name]-[hash].js`,
+                  }
+          }
+    }
 })
