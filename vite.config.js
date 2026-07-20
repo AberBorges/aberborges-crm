759371b8e64080f1b933d001dec53c13109c6fa2
AberBorges <aberturasborgesapp@gmail.com>
2026-07-20 11:28:47 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// v5 - offline localStorage sync
 export default defineConfig({
-    plugins: [react()],
-    build: {
-          rollupOptions: {
-                  output: {
-                            entryFileNames: `assets/[name]-[hash].js`,
-                            chunkFileNames: `assets/[name]-[hash].js`,
-                  }
-          }
-    }
+      plugins: [react()],
+      build: {
+              minify: false,
+              rollupOptions: {
+                        treeshake: false
+              }
+      }
 })
